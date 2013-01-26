dd7bbdc4d916df586f7783506dd2e62d47693c72
Steve Canny <scanny@cisco.com>
2013-01-26 03:36:45 -0800

    add sphinx-bootstrap Sphinx theme

--- doc/conf.py
+++ doc/conf.py
@@ -99,24 +99,26 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
-# html_theme = 'sphinx-bootstrap'
+# html_theme = 'default'
+html_theme = 'sphinx-bootstrap'
+# html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
-# html_theme_options =\
-#     { 'github_user'      : 'scanny'
-#     , 'github_repo'      : 'python-pptx'
-#     # , 'analytics_code'   : 'UA-00000000-1'
-#     # , 'twitter_username' : 'scotchmedia'
-#     # , 'home_url'         : 'http://code.scotchmedia.com/sphinx-bootstrap'
-#     # , 'disqus_shortname' : 'scotchmedia'
-#     }
+# html_theme_options = {}
+html_theme_options =\
+    { 'github_user'      : 'scanny'
+    , 'github_repo'      : 'python-pptx'
+    # , 'analytics_code'   : 'UA-00000000-1'
+    , 'twitter_username' : 'scanny'
+    , 'home_url'         : 'https://github.com/scanny/python-pptx'
+    # , 'disqus_shortname' : 'scotchmedia'
+    }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = ['./themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
