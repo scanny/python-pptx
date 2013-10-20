8b0bcea0e69cdd04994a724d9bbe1094f3cd145b
Steve Canny <scanny@cisco.com>
2013-10-19 21:39:04 -0700

    reorg: move _RelationshipCollection to rels.py

--- pptx/rels.py
+++ pptx/rels.py
@@ -4,6 +4,8 @@
 Relationship-related objects.
 """
 
+from pptx.util import Collection, Partname
+
 
 class _Relationship(object):
     """
@@ -59,3 +61,119 @@ class _Relationship(object):
     @_rId.setter
     def _rId(self, value):
         self.__rId = value
+
+
+class _RelationshipCollection(Collection):
+    """
+    Sequence of relationships maintained in rId order. Maintaining the
+    relationships in sorted order makes the .rels files both repeatable and
+    more readable, which is very helpful for debugging.
+    |_RelationshipCollection| has an attribute *_reltype_ordering* which is a
+    sequence (tuple) of reltypes. If *_reltype_ordering* contains one or more
+    reltype, the collection is maintained in reltype + partname.idx order and
+    relationship ids (rIds) are renumbered to match that sequence and any
+    numbering gaps are filled in.
+    """
+    def __init__(self):
+        super(_RelationshipCollection, self).__init__()
+        self.__reltype_ordering = ()
+
+    def _additem(self, relationship):
+        """
+        Insert *relationship* into the appropriate position in this ordered
+        collection.
+        """
+        rIds = [rel._rId for rel in self]
+        if relationship._rId in rIds:
+            tmpl = "cannot add relationship with duplicate rId '%s'"
+            raise ValueError(tmpl % relationship._rId)
+        self._values.append(relationship)
+        self.__resequence()
+        # register as observer of partname changes
+        relationship._target.add_observer(self)
+
+    @property
+    def _next_rId(self):
+        """
+        Next available rId in collection, starting from 'rId1' and making use
+        of any gaps in numbering, e.g. 'rId2' for rIds ['rId1', 'rId3'].
+        """
+        tmpl = 'rId%d'
+        next_rId_num = 1
+        for relationship in self._values:
+            if relationship._num > next_rId_num:
+                return tmpl % next_rId_num
+            next_rId_num += 1
+        return tmpl % next_rId_num
+
+    def related_part(self, reltype):
+        """
+        Return first part in collection having relationship type *reltype* or
+        raise |KeyError| if not found.
+        """
+        for relationship in self._values:
+            if relationship._reltype == reltype:
+                return relationship._target
+        tmpl = "no related part with relationship type '%s'"
+        raise KeyError(tmpl % reltype)
+
+    @property
+    def _reltype_ordering(self):
+        """
+        Tuple of relationship types, e.g. ``(RT.SLIDE, RT.SLIDE_LAYOUT)``. If
+        present, relationships of those types are grouped, and those groups
+        are ordered in the same sequence they appear in the tuple. In
+        addition, relationships of the same type are sequenced in order of
+        partname number (e.g. 16 for /ppt/slides/slide16.xml). If empty, the
+        collection is maintained in existing rId order; rIds are not
+        renumbered and any gaps in numbering are left to remain. Specifying
+        this value for a collection with members causes it to be immediately
+        reordered. The ordering is maintained as relationships are added or
+        removed, renumbering rIds whenever necessary to also maintain the
+        sequence in rId order.
+        """
+        return self.__reltype_ordering
+
+    @_reltype_ordering.setter
+    def _reltype_ordering(self, ordering):
+        self.__reltype_ordering = tuple(ordering)
+        self.__resequence()
+
+    def rels_of_reltype(self, reltype):
+        """
+        Return a :class:`list` containing the subset of relationships in this
+        collection of type *reltype*. The returned list is ordered by rId.
+        Returns an empty list if there are no relationships of type *reltype*
+        in the collection.
+        """
+        return [rel for rel in self._values if rel._reltype == reltype]
+
+    def notify(self, subject, name, value):
+        """RelationshipCollection implements the Observer interface"""
+        if name == 'partname':
+            self.__resequence()
+
+    def __resequence(self):
+        """
+        Sort relationships and renumber if necessary to maintain values in rId
+        order.
+        """
+        if self.__reltype_ordering:
+            def reltype_key(rel):
+                reltype = rel._reltype
+                if reltype in self.__reltype_ordering:
+                    return self.__reltype_ordering.index(reltype)
+                return len(self.__reltype_ordering)
+
+            def partname_idx_key(rel):
+                partname = Partname(rel._target.partname)
+                if partname.idx is None:
+                    return 0
+                return partname.idx
+            self._values.sort(key=lambda rel: partname_idx_key(rel))
+            self._values.sort(key=lambda rel: reltype_key(rel))
+            # renumber consistent with new sort order
+            for idx, relationship in enumerate(self._values):
+                relationship._rId = 'rId%d' % (idx+1)
+        else:
+            self._values.sort(key=lambda rel: rel._num)
--- tests/test_rels.py
+++ tests/test_rels.py
@@ -2,12 +2,29 @@
 
 """Test suite for pptx.part module."""
 
-from hamcrest import assert_that, equal_to, is_
+from hamcrest import assert_that, equal_to, is_, same_instance
+from mock import Mock
 
-from pptx.rels import _Relationship
+from pptx.rels import _Relationship, _RelationshipCollection
 from pptx.opc_constants import RELATIONSHIP_TYPE as RT
+from pptx.presentation import _BasePart
 
 from testing import TestCase
+
+
+class PartBuilder(object):
+    """Builder class for test Parts"""
+    def __init__(self):
+        self.partname = '/ppt/slides/slide1.xml'
+
+    def with_partname(self, partname):
+        self.partname = partname
+        return self
+
+    def build(self):
+        p = _BasePart()
+        p.partname = self.partname
+        return p
 
 
 class Test_Relationship(TestCase):
@@ -49,4 +66,208 @@ class Test_Relationship(TestCase):
         expected = rId
         actual = self.rel._rId
         msg = "expected '%s', got '%s'" % (expected, actual)
+        self.assertEqual(expected, actual, msg)
+
+
+class Test_RelationshipCollection(TestCase):
+    """Test _RelationshipCollection"""
+    def setUp(self):
+        self.relationships = _RelationshipCollection()
+
+    def __reltype_ordering_mock(self):
+        """
+        Return RelationshipCollection instance with mocked-up contents
+        suitable for testing _reltype_ordering.
+        """
+        # setup ------------------------
+        partnames = ['/ppt/slides/slide4.xml',
+                     '/ppt/slideLayouts/slideLayout1.xml',
+                     '/ppt/slideMasters/slideMaster1.xml',
+                     '/ppt/slides/slide1.xml',
+                     '/ppt/presProps.xml']
+        part1 = Mock(name='part1')
+        part1.partname = partnames[0]
+        part2 = Mock(name='part2')
+        part2.partname = partnames[1]
+        part3 = Mock(name='part3')
+        part3.partname = partnames[2]
+        part4 = Mock(name='part4')
+        part4.partname = partnames[3]
+        part5 = Mock(name='part5')
+        part5.partname = partnames[4]
+        rel1 = _Relationship('rId1', RT.SLIDE,        part1)
+        rel2 = _Relationship('rId2', RT.SLIDE_LAYOUT, part2)
+        rel3 = _Relationship('rId3', RT.SLIDE_MASTER, part3)
+        rel4 = _Relationship('rId4', RT.SLIDE,        part4)
+        rel5 = _Relationship('rId5', RT.PRES_PROPS,   part5)
+        relationships = _RelationshipCollection()
+        relationships._additem(rel1)
+        relationships._additem(rel2)
+        relationships._additem(rel3)
+        relationships._additem(rel4)
+        relationships._additem(rel5)
+        return (relationships, partnames)
+
+    def test_it_can_find_related_part(self):
+        """_RelationshipCollection can find related part"""
+        # setup ------------------------
+        reltype = RT.CORE_PROPERTIES
+        part = Mock(name='part')
+        relationship = _Relationship('rId1', reltype, part)
+        relationships = _RelationshipCollection()
+        relationships._additem(relationship)
+        # exercise ---------------------
+        retval = relationships.related_part(reltype)
+        # verify -----------------------
+        assert_that(retval, same_instance(part))
+
+    def test_it_raises_if_it_cant_find_a_related_part(self):
+        """_RelationshipCollection raises if it can't find a related part"""
+        # setup ------------------------
+        relationships = _RelationshipCollection()
+        # exercise ---------------------
+        with self.assertRaises(KeyError):
+            relationships.related_part('foobar')
+
+    def test__additem_raises_on_dup_rId(self):
+        """_RelationshipCollection._additem raises on duplicate rId"""
+        # setup ------------------------
+        part1 = _BasePart()
+        part2 = _BasePart()
+        rel1 = _Relationship('rId9', None, part1)
+        rel2 = _Relationship('rId9', None, part2)
+        self.relationships._additem(rel1)
+        # verify -----------------------
+        with self.assertRaises(ValueError):
+            self.relationships._additem(rel2)
+
+    def test__additem_maintains_rId_ordering(self):
+        """_RelationshipCollection maintains rId ordering on additem()"""
+        # setup ------------------------
+        part1 = _BasePart()
+        part2 = _BasePart()
+        part3 = _BasePart()
+        rel1 = _Relationship('rId1', None, part1)
+        rel2 = _Relationship('rId2', None, part2)
+        rel3 = _Relationship('rId3', None, part3)
+        # exercise ---------------------
+        self.relationships._additem(rel2)
+        self.relationships._additem(rel1)
+        self.relationships._additem(rel3)
+        # verify -----------------------
+        expected = ['rId1', 'rId2', 'rId3']
+        actual = [rel._rId for rel in self.relationships]
+        msg = "expected ordering %s, got %s" % (expected, actual)
+        self.assertEqual(expected, actual, msg)
+
+    def test__additem_maintains_reltype_ordering(self):
+        """_RelationshipCollection maintains reltype ordering on additem()"""
+        # setup ------------------------
+        relationships, partnames = self.__reltype_ordering_mock()
+        ordering = (RT.SLIDE_MASTER, RT.SLIDE_LAYOUT, RT.SLIDE)
+        relationships._reltype_ordering = ordering
+        partname = '/ppt/slides/slide2.xml'
+        part = Mock(name='new_part')
+        part.partname = partname
+        rId = relationships._next_rId
+        rel = _Relationship(rId, RT.SLIDE, part)
+        # exercise ---------------------
+        relationships._additem(rel)
+        # verify ordering -------------
+        expected = [partnames[2], partnames[1], partnames[3],
+                    partname, partnames[0], partnames[4]]
+        actual = [r._target.partname for r in relationships]
+        msg = "expected ordering %s, got %s" % (expected, actual)
+        self.assertEqual(expected, actual, msg)
+
+    def test_rels_of_reltype_return_value(self):
+        """RelationshipCollection._rels_of_reltype returns correct rels"""
+        # setup ------------------------
+        relationships, partnames = self.__reltype_ordering_mock()
+        # exercise ---------------------
+        retval = relationships.rels_of_reltype(RT.SLIDE)
+        # verify ordering -------------
+        expected = ['rId1', 'rId4']
+        actual = [rel._rId for rel in retval]
+        msg = "expected %s, got %s" % (expected, actual)
+        self.assertEqual(expected, actual, msg)
+
+    def test__reltype_ordering_sorts_rels(self):
+        """RelationshipCollection._reltype_ordering sorts rels"""
+        # setup ------------------------
+        relationships, partnames = self.__reltype_ordering_mock()
+        ordering = (RT.SLIDE_MASTER, RT.SLIDE_LAYOUT, RT.SLIDE)
+        # exercise ---------------------
+        relationships._reltype_ordering = ordering
+        # verify ordering -------------
+        assert_that(relationships._reltype_ordering, is_(equal_to(ordering)))
+        expected = [partnames[2], partnames[1], partnames[3], partnames[0],
+                    partnames[4]]
+        actual = [rel._target.partname for rel in relationships]
+        msg = "expected ordering %s, got %s" % (expected, actual)
+        self.assertEqual(expected, actual, msg)
+
+    def test__reltype_ordering_renumbers_rels(self):
+        """RelationshipCollection._reltype_ordering renumbers rels"""
+        # setup ------------------------
+        relationships, partnames = self.__reltype_ordering_mock()
+        ordering = (RT.SLIDE_MASTER, RT.SLIDE_LAYOUT, RT.SLIDE)
+        # exercise ---------------------
+        relationships._reltype_ordering = ordering
+        # verify renumbering ----------
+        expected = ['rId1', 'rId2', 'rId3', 'rId4', 'rId5']
+        actual = [rel._rId for rel in relationships]
+        msg = "expected numbering %s, got %s" % (expected, actual)
+        self.assertEqual(expected, actual, msg)
+
+    def test__next_rId_fills_gap(self):
+        """_RelationshipCollection._next_rId fills gap in rId sequence"""
+        # setup ------------------------
+        part1 = _BasePart()
+        part2 = _BasePart()
+        part3 = _BasePart()
+        part4 = _BasePart()
+        rel1 = _Relationship('rId1', None, part1)
+        rel2 = _Relationship('rId2', None, part2)
+        rel3 = _Relationship('rId3', None, part3)
+        rel4 = _Relationship('rId4', None, part4)
+        cases = (('rId1', (rel2, rel3, rel4)),
+                 ('rId2', (rel1, rel3, rel4)),
+                 ('rId3', (rel1, rel2, rel4)),
+                 ('rId4', (rel1, rel2, rel3)))
+        # exercise ---------------------
+        expected_rIds = []
+        actual_rIds = []
+        for expected_rId, rels in cases:
+            expected_rIds.append(expected_rId)
+            relationships = _RelationshipCollection()
+            for rel in rels:
+                relationships._additem(rel)
+            actual_rIds.append(relationships._next_rId)
+        # verify -----------------------
+        expected = expected_rIds
+        actual = actual_rIds
+        msg = "expected rIds %s, got %s" % (expected, actual)
+        self.assertEqual(expected, actual, msg)
+
+    def test_reorders_on_partname_change(self):
+        """RelationshipCollection reorders on partname change"""
+        # setup ------------------------
+        partname1 = '/ppt/slides/slide1.xml'
+        partname2 = '/ppt/slides/slide2.xml'
+        partname3 = '/ppt/slides/slide3.xml'
+        part1 = PartBuilder().with_partname(partname1).build()
+        part2 = PartBuilder().with_partname(partname2).build()
+        rel1 = _Relationship('rId1', RT.SLIDE, part1)
+        rel2 = _Relationship('rId2', RT.SLIDE, part2)
+        relationships = _RelationshipCollection()
+        relationships._reltype_ordering = (RT.SLIDE)
+        relationships._additem(rel1)
+        relationships._additem(rel2)
+        # exercise ---------------------
+        part1.partname = partname3
+        # verify -----------------------
+        expected = [partname2, partname3]
+        actual = [rel._target.partname for rel in relationships]
+        msg = "expected ordering %s, got %s" % (expected, actual)
         self.assertEqual(expected, actual, msg)
