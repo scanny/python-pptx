70f6270138624abca59c07cbe7414ec142c9aed0
Steve Canny <scanny@cisco.com>
2013-10-26 14:32:19 -0700

    docs: mv _build dir to .build

--- docs/conf.py
+++ docs/conf.py
@@ -165,7 +165,7 @@ rst_epilog = """
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['.build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
