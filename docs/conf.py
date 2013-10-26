6e45d88d772c1a1edc76967cd6d64e0fb9388b2d
Steve Canny <scanny@cisco.com>
2013-10-26 01:19:24 -0700

    docs: switch to armstrong Sphinx template

--- docs/conf.py
+++ docs/conf.py
@@ -189,24 +189,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'default'
-html_theme = 'sphinx-bootstrap'
-# html_theme = 'bootstrap'
+html_theme = 'armstrong'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 # html_theme_options = {}
-html_theme_options = {
-    'github_user':      'scanny',
-    'github_repo':      'python-pptx',
-    'twitter_username': 'scanny',
-    'home_url':         'https://github.com/scanny/python-pptx',
-}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = ['./themes']
+html_theme_path = ['.themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
