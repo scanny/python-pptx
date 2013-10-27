73734951516f252c06d667c037339f9713c8f02a
Steve Canny <scanny@cisco.com>
2013-10-27 03:34:19 -0700

    reorg: extract _NamespacePrefixedTag to oxml

--- pptx/oxml/util.py
+++ pptx/oxml/util.py
@@ -8,24 +8,3 @@ slide.
 """
 
 from __future__ import absolute_import
-
-
-class _NamespacePrefixedTag(str):
-    """
-    Value object that knows the semantics of an XML tag having a namespace
-    prefix.
-    """
-    def __new__(cls, nstag, *args):
-        return super(_NamespacePrefixedTag, cls).__new__(cls, nstag)
-
-    def __init__(self, nstag, prefix_to_uri_map):
-        self._pfx, self._local_part = nstag.split(':')
-        self._ns_uri = prefix_to_uri_map[self._pfx]
-
-    @property
-    def clark_name(self):
-        return '{%s}%s' % (self._ns_uri, self._local_part)
-
-    @property
-    def namespace_map(self):
-        return {self._pfx: self._ns_uri}
