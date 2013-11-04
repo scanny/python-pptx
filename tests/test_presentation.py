0e1eefd09f9ea4c76578b6c922c5f5b94e543279
Steve Canny <scanny@cisco.com>
2013-11-03 18:46:19 -0800

    prs: add robust test for Presentation.slides property

--- tests/test_presentation.py
+++ tests/test_presentation.py
@@ -11,13 +11,15 @@ from mock import Mock
 
 from pptx.exceptions import InvalidPackageError
 from pptx.opc.constants import CONTENT_TYPE as CT, RELATIONSHIP_TYPE as RT
+from pptx.opc.rels import RelationshipCollection
 from pptx.oxml.ns import namespaces
+from pptx.oxml.presentation import CT_Presentation, CT_SlideIdList
 from pptx.parts.coreprops import CoreProperties
 from pptx.parts.part import PartCollection
 from pptx.parts.slides import Slide, SlideCollection, SlideLayout, SlideMaster
 from pptx.presentation import Package, Part, Presentation
 
-from .unitutil import absjoin, test_file_dir
+from .unitutil import absjoin, class_mock, instance_mock, test_file_dir
 
 
 images_pptx_path = absjoin(test_file_dir, 'with_images.pptx')
@@ -135,13 +137,50 @@ class DescribePresentation(object):
     def it_provides_access_to_the_slide_masters(self, prs):
         assert isinstance(prs.slidemasters, PartCollection)
 
-    def it_provides_access_to_the_slides(self, prs):
-        assert isinstance(prs.slides, SlideCollection)
+    def it_creates_slide_collection_on_first_reference(
+            self, prs, SlideCollection_, sldIdLst_, rels_, slides_):
+        slides = prs.slides
+        # verify -----------------------
+        prs._element.get_or_add_sldIdLst.assert_called_once_with()
+        SlideCollection_.assert_called_once_with(sldIdLst_, rels_, prs)
+        assert slides == slides_
+
+    def it_reuses_slide_collection_instance_on_later_references(self, prs):
+        slides_1 = prs.slides
+        slides_2 = prs.slides
+        assert slides_2 is slides_1
 
     # fixtures ---------------------------------------------
 
     @pytest.fixture
-    def prs(self):
+    def ct_presentation_(self, request, sldIdLst_):
+        ct_presentation_ = instance_mock(request, CT_Presentation)
+        ct_presentation_.get_or_add_sldIdLst.return_value = sldIdLst_
+        return ct_presentation_
+
+    @pytest.fixture
+    def prs(self, ct_presentation_, rels_):
         prs = Presentation()
-        prs._element = Mock(name='_element')
+        prs._element = ct_presentation_
+        prs._relationships = rels_
         return prs
+
+    @pytest.fixture
+    def rels_(self, request):
+        return instance_mock(request, RelationshipCollection)
+
+    @pytest.fixture
+    def sldIdLst_(self, request):
+        return instance_mock(request, CT_SlideIdList)
+
+    @pytest.fixture
+    def SlideCollection_(self, request, slides_):
+        SlideCollection_ = class_mock(
+            request, 'pptx.presentation.SlideCollection'
+        )
+        SlideCollection_.return_value = slides_
+        return SlideCollection_
+
+    @pytest.fixture
+    def slides_(self, request):
+        return instance_mock(request, SlideCollection)
