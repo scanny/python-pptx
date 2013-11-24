29a643f1200350cae08c24ab6e67b923a8ccc31b
Steve Canny <scanny@cisco.com>
2013-11-24 03:57:20 -0800

    opc: transplant opc.RelationshipCollection

--- pptx/opc/package.py
+++ pptx/opc/package.py
@@ -7,6 +7,8 @@ writing presentations to and from a .pptx file.
 
 from __future__ import absolute_import
 
+from pptx.opc.oxml import CT_Relationships
+
 
 class PartFactory(object):
     """
@@ -22,6 +24,68 @@ class PartFactory(object):
             CustomPartClass = cls.part_type_for[content_type]
             return CustomPartClass.load(partname, content_type, blob)
         return cls.default_part_type(partname, content_type, blob)
+
+
+class RelationshipCollection(object):
+    """
+    Collection object for |_Relationship| instances, having list semantics.
+    """
+    def __init__(self, baseURI):
+        super(RelationshipCollection, self).__init__()
+        self._baseURI = baseURI
+        self._rels = []
+
+    def __getitem__(self, key):
+        """
+        Implements access by subscript, e.g. ``rels[9]``. It also implements
+        dict-style lookup of a relationship by rId, e.g. ``rels['rId1']``.
+        """
+        if isinstance(key, basestring):
+            for rel in self._rels:
+                if rel.rId == key:
+                    return rel
+            raise KeyError("no rId '%s' in RelationshipCollection" % key)
+        else:
+            return self._rels.__getitem__(key)
+
+    def __len__(self):
+        """Implements len() built-in on this object"""
+        return self._rels.__len__()
+
+    def add_relationship(self, reltype, target, rId, external=False):
+        """
+        Return a newly added |_Relationship| instance.
+        """
+        rel = _Relationship(rId, reltype, target, self._baseURI, external)
+        self._rels.append(rel)
+        return rel
+
+    def get_rel_of_type(self, reltype):
+        """
+        Return single relationship of type *reltype* from the collection.
+        Raises |KeyError| if no matching relationship is found. Raises
+        |ValueError| if more than one matching relationship is found.
+        """
+        matching = [rel for rel in self._rels if rel.reltype == reltype]
+        if len(matching) == 0:
+            tmpl = "no relationship of type '%s' in collection"
+            raise KeyError(tmpl % reltype)
+        if len(matching) > 1:
+            tmpl = "multiple relationships of type '%s' in collection"
+            raise ValueError(tmpl % reltype)
+        return matching[0]
+
+    @property
+    def xml(self):
+        """
+        Serialize this relationship collection into XML suitable for storage
+        as a .rels file in an OPC package.
+        """
+        rels_elm = CT_Relationships.new()
+        for rel in self._rels:
+            rels_elm.add_rel(rel.rId, rel.reltype, rel.target_ref,
+                             rel.is_external)
+        return rels_elm.xml
 
 
 class Unmarshaller(object):
@@ -67,3 +131,42 @@ class Unmarshaller(object):
                       else parts[srel.target_partname])
             source._add_relationship(srel.reltype, target, srel.rId,
                                      srel.is_external)
+
+
+class _Relationship(object):
+    """
+    Value object for relationship to part.
+    """
+    def __init__(self, rId, reltype, target, baseURI, external=False):
+        super(_Relationship, self).__init__()
+        self._rId = rId
+        self._reltype = reltype
+        self._target = target
+        self._baseURI = baseURI
+        self._is_external = bool(external)
+
+    @property
+    def is_external(self):
+        return self._is_external
+
+    @property
+    def reltype(self):
+        return self._reltype
+
+    @property
+    def rId(self):
+        return self._rId
+
+    @property
+    def target_part(self):
+        if self._is_external:
+            raise ValueError("target_part property on _Relationship is undef"
+                             "ined when target mode is External")
+        return self._target
+
+    @property
+    def target_ref(self):
+        if self._is_external:
+            return self._target
+        else:
+            return self._target.partname.relative_ref(self._baseURI)
--- tests/opc/test_package.py
+++ tests/opc/test_package.py
@@ -8,11 +8,15 @@ from __future__ import absolute_import
 
 import pytest
 
-from mock import call, Mock
+from mock import call, Mock, patch, PropertyMock
 
-from pptx.opc.package import Unmarshaller
+from pptx.opc.oxml import CT_Relationships
+from pptx.opc.packuri import PackURI
+from pptx.opc.package import (
+    _Relationship, RelationshipCollection, Unmarshaller
+)
 
-from ..unitutil import absjoin, method_mock, test_file_dir
+from ..unitutil import absjoin, class_mock, method_mock, test_file_dir
 
 
 test_pptx_path = absjoin(test_file_dir, 'test.pptx')
@@ -61,6 +65,140 @@ def tmp_pptx_path(tmpdir):
 #         f.write(stream.read())
 #     msg = "Package.save(stream) did not create zipfile"
 #     assert is_zipfile(tmp_pptx_path), msg
+
+
+class Describe_Relationship(object):
+
+    def it_remembers_construction_values(self):
+        # test data --------------------
+        rId = 'rId9'
+        reltype = 'reltype'
+        target = Mock(name='target_part')
+        external = False
+        # exercise ---------------------
+        rel = _Relationship(rId, reltype, target, None, external)
+        # verify -----------------------
+        assert rel.rId == rId
+        assert rel.reltype == reltype
+        assert rel.target_part == target
+        assert rel.is_external == external
+
+    def it_should_raise_on_target_part_access_on_external_rel(self):
+        rel = _Relationship(None, None, None, None, external=True)
+        with pytest.raises(ValueError):
+            rel.target_part
+
+    def it_should_have_target_ref_for_external_rel(self):
+        rel = _Relationship(None, None, 'target', None, external=True)
+        assert rel.target_ref == 'target'
+
+    def it_should_have_relative_ref_for_internal_rel(self):
+        """
+        Internal relationships (TargetMode == 'Internal' in the XML) should
+        have a relative ref, e.g. '../slideLayouts/slideLayout1.xml', for
+        the target_ref attribute.
+        """
+        part = Mock(name='part', partname=PackURI('/ppt/media/image1.png'))
+        baseURI = '/ppt/slides'
+        rel = _Relationship(None, None, part, baseURI)  # external=False
+        assert rel.target_ref == '../media/image1.png'
+
+
+class DescribeRelationshipCollection(object):
+
+    def it_has_a_len(self):
+        rels = RelationshipCollection(None)
+        assert len(rels) == 0
+
+    def it_supports_indexed_access(self):
+        rels = RelationshipCollection(None)
+        try:
+            rels[0]
+        except TypeError:
+            msg = 'RelationshipCollection does not support indexed access'
+            pytest.fail(msg)
+        except IndexError:
+            pass
+
+    def it_has_dict_style_lookup_of_rel_by_rId(self):
+        rel = Mock(name='rel', rId='foobar')
+        rels = RelationshipCollection(None)
+        rels._rels.append(rel)
+        assert rels['foobar'] == rel
+
+    def it_should_raise_on_failed_lookup_by_rId(self):
+        rel = Mock(name='rel', rId='foobar')
+        rels = RelationshipCollection(None)
+        rels._rels.append(rel)
+        with pytest.raises(KeyError):
+            rels['barfoo']
+
+    def it_can_add_a_relationship(self, _Relationship_):
+        baseURI, rId, reltype, target, external = (
+            'baseURI', 'rId9', 'reltype', 'target', False
+        )
+        rels = RelationshipCollection(baseURI)
+        rel = rels.add_relationship(reltype, target, rId, external)
+        _Relationship_.assert_called_once_with(rId, reltype, target, baseURI,
+                                               external)
+        assert rels[0] == rel
+        assert rel == _Relationship_.return_value
+
+    def it_can_compose_rels_xml(self, rels, rels_elm):
+        # exercise ---------------------
+        rels.xml
+        # trace ------------------------
+        print('Actual calls:\n%s' % rels_elm.mock_calls)
+        # verify -----------------------
+        expected_rels_elm_calls = [
+            call.add_rel('rId1', 'http://rt-hyperlink', 'http://some/link',
+                         True),
+            call.add_rel('rId2', 'http://rt-image', '../media/image1.png',
+                         False),
+            call.xml()
+        ]
+        assert rels_elm.mock_calls == expected_rels_elm_calls
+
+    # fixtures ---------------------------------------------
+
+    @pytest.fixture
+    def _Relationship_(self, request):
+        return class_mock(request, 'pptx.opc.package._Relationship')
+
+    @pytest.fixture
+    def rels(self):
+        """
+        Populated RelationshipCollection instance that will exercise the
+        rels.xml property.
+        """
+        rels = RelationshipCollection('/baseURI')
+        rels.add_relationship(
+            reltype='http://rt-hyperlink', target='http://some/link',
+            rId='rId1', external=True
+        )
+        part = Mock(name='part')
+        part.partname.relative_ref.return_value = '../media/image1.png'
+        rels.add_relationship(reltype='http://rt-image', target=part,
+                              rId='rId2')
+        return rels
+
+    @pytest.fixture
+    def rels_elm(self, request):
+        """
+        Return a rels_elm mock that will be returned from
+        CT_Relationships.new()
+        """
+        # create rels_elm mock with a .xml property
+        rels_elm = Mock(name='rels_elm')
+        xml = PropertyMock(name='xml')
+        type(rels_elm).xml = xml
+        rels_elm.attach_mock(xml, 'xml')
+        rels_elm.reset_mock()  # to clear attach_mock call
+        # patch CT_Relationships to return that rels_elm
+        patch_ = patch.object(CT_Relationships, 'new', return_value=rels_elm)
+        patch_.start()
+        request.addfinalizer(patch_.stop)
+        return rels_elm
 
 
 class DescribeUnmarshaller(object):
