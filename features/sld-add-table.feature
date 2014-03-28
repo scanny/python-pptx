0075e152f1e66701c670ca6b53dc95aca8bf7d21
Steve Canny <scanny@cisco.com>
2014-03-27 21:40:29 -0700

    acpt: use .shapes_new in sld-add-table.feature
    Also update a couple names to current conventions

--- features/sld-add-table.feature
+++ features/sld-add-table.feature
@@ -3,6 +3,7 @@ Feature: Add a table to a slide
   As a presentation developer
   I need the ability to place a table on a slide
 
+  @wip
   Scenario: Add a table to a slide
      Given a blank slide
       When I add a table to the slide's shape collection
@@ -10,13 +11,13 @@ Feature: Add a table to a slide
       Then the table appears in the slide
 
   Scenario: Set column widths
-     Given I have a reference to a table
+     Given a table
       When I set the width of the table's columns
        And I save the presentation
       Then the table appears with the new column widths
 
   Scenario: Set cell text
-     Given I have a reference to a table
+     Given a table
       When I set the text of the first cell
        And I save the presentation
       Then the text appears in the first cell of the table
--- features/steps/table.py
+++ features/steps/table.py
@@ -20,8 +20,8 @@ from .helpers import saved_pptx_path
 # given ===================================================
 
 
-@given('I have a reference to a table')
-def step_given_ref_to_table(context):
+@given('a table')
+def given_a_table(context):
     context.prs = Presentation()
     slide_layout = context.prs.slide_layouts[6]
     sld = context.prs.slides.add_slide(slide_layout)
@@ -45,8 +45,8 @@ def given_a_table_cell(context):
 
 
 @when("I add a table to the slide's shape collection")
-def step_when_add_table(context):
-    shapes = context.sld.shapes
+def when_add_table(context):
+    shapes = context.sld.shapes_new
     x, y = (Inches(1.00), Inches(2.00))
     cx, cy = (Inches(3.00), Inches(1.00))
     shapes.add_table(2, 2, x, y, cx, cy)
@@ -63,7 +63,7 @@ def when_set_cell_fill_type_to_solid(context):
 
 
 @when("I set the cell margins")
-def step_when_set_cell_margins(context):
+def when_set_cell_margins(context):
     context.cell.margin_top = 1000
     context.cell.margin_right = 2000
     context.cell.margin_bottom = 3000
@@ -71,47 +71,47 @@ def step_when_set_cell_margins(context):
 
 
 @when("I set the cell vertical anchor to middle")
-def step_when_set_cell_vertical_anchor_to_middle(context):
+def when_set_cell_vertical_anchor_to_middle(context):
     context.cell.vertical_anchor = MSO.ANCHOR_MIDDLE
 
 
 @when("I set the first_col property to True")
-def step_when_set_first_col_property_to_true(context):
+def when_set_first_col_property_to_true(context):
     context.tbl.first_col = True
 
 
 @when("I set the first_row property to True")
-def step_when_set_first_row_property_to_true(context):
+def when_set_first_row_property_to_true(context):
     context.tbl.first_row = True
 
 
 @when("I set the horz_banding property to True")
-def step_when_set_horz_banding_property_to_true(context):
+def when_set_horz_banding_property_to_true(context):
     context.tbl.horz_banding = True
 
 
 @when("I set the last_col property to True")
-def step_when_set_last_col_property_to_true(context):
+def when_set_last_col_property_to_true(context):
     context.tbl.last_col = True
 
 
 @when("I set the last_row property to True")
-def step_when_set_last_row_property_to_true(context):
+def when_set_last_row_property_to_true(context):
     context.tbl.last_row = True
 
 
 @when("I set the text of the first cell")
-def step_when_set_text_of_first_cell(context):
+def when_set_text_of_first_cell(context):
     context.tbl.cell(0, 0).text = 'test text'
 
 
 @when("I set the vert_banding property to True")
-def step_when_set_vert_banding_property_to_true(context):
+def when_set_vert_banding_property_to_true(context):
     context.tbl.vert_banding = True
 
 
 @when("I set the width of the table's columns")
-def step_when_set_table_column_widths(context):
+def when_set_table_column_widths(context):
     context.tbl.columns[0].width = Inches(1.50)
     context.tbl.columns[1].width = Inches(3.00)
 
@@ -120,7 +120,7 @@ def step_when_set_table_column_widths(context):
 
 
 @then('the cell contents are inset by the margins')
-def step_then_cell_contents_are_inset_by_the_margins(context):
+def then_cell_contents_are_inset_by_the_margins(context):
     prs = Presentation(saved_pptx_path)
     table = prs.slides[0].shapes[0]
     cell = table.cell(0, 0)
@@ -131,7 +131,7 @@ def step_then_cell_contents_are_inset_by_the_margins(context):
 
 
 @then('the cell contents are vertically centered')
-def step_then_cell_contents_are_vertically_centered(context):
+def then_cell_contents_are_vertically_centered(context):
     prs = Presentation(saved_pptx_path)
     table = prs.slides[0].shapes[0]
     cell = table.cell(0, 0)
@@ -139,19 +139,19 @@ def step_then_cell_contents_are_vertically_centered(context):
 
 
 @then('the columns of the table have alternating shading')
-def step_then_columns_of_table_have_alternating_shading(context):
+def then_columns_of_table_have_alternating_shading(context):
     tbl = Presentation(saved_pptx_path).slides[0].shapes[0]
     assert_that(tbl.vert_banding, is_(True))
 
 
 @then('the first column of the table has special formatting')
-def step_then_first_column_of_table_has_special_formatting(context):
+def then_first_column_of_table_has_special_formatting(context):
     tbl = Presentation(saved_pptx_path).slides[0].shapes[0]
     assert_that(tbl.first_col, is_(True))
 
 
 @then('the first row of the table has special formatting')
-def step_then_first_row_of_table_has_special_formatting(context):
+def then_first_row_of_table_has_special_formatting(context):
     tbl = Presentation(saved_pptx_path).slides[0].shapes[0]
     assert_that(tbl.first_row, is_(True))
 
@@ -162,25 +162,25 @@ def then_cell_fore_color_is_RGB_value_I_set(context):
 
 
 @then('the last column of the table has special formatting')
-def step_then_last_column_of_table_has_special_formatting(context):
+def then_last_column_of_table_has_special_formatting(context):
     tbl = Presentation(saved_pptx_path).slides[0].shapes[0]
     assert_that(tbl.last_col, is_(True))
 
 
 @then('the last row of the table has special formatting')
-def step_then_last_row_of_table_has_special_formatting(context):
+def then_last_row_of_table_has_special_formatting(context):
     tbl = Presentation(saved_pptx_path).slides[0].shapes[0]
     assert_that(tbl.last_row, is_(True))
 
 
 @then('the rows of the table have alternating shading')
-def step_then_rows_of_table_have_alternating_shading(context):
+def then_rows_of_table_have_alternating_shading(context):
     tbl = Presentation(saved_pptx_path).slides[0].shapes[0]
     assert_that(tbl.horz_banding, is_(True))
 
 
 @then('the table appears in the slide')
-def step_then_table_appears_in_slide(context):
+def then_table_appears_in_slide(context):
     prs = Presentation(saved_pptx_path)
     sld = prs.slides[0]
     shapes = sld.shapes
@@ -189,7 +189,7 @@ def step_then_table_appears_in_slide(context):
 
 
 @then('the table appears with the new column widths')
-def step_then_table_appears_with_new_col_widths(context):
+def then_table_appears_with_new_col_widths(context):
     prs = Presentation(saved_pptx_path)
     sld = prs.slides[0]
     tbl = sld.shapes[0]
@@ -198,7 +198,7 @@ def step_then_table_appears_with_new_col_widths(context):
 
 
 @then('the text appears in the first cell of the table')
-def step_then_text_appears_in_first_cell_of_table(context):
+def then_text_appears_in_first_cell_of_table(context):
     prs = Presentation(saved_pptx_path)
     sld = prs.slides[0]
     tbl = sld.shapes[0]
