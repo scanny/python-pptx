89854662d177d019d56476ee23a5e1b17beb5bea
Steve Canny <scanny@cisco.com>
2014-06-08 00:43:53 -0700

    oxml: convert CT_Connector to xmlchemy

--- pptx/oxml/shapes/connector.py
+++ pptx/oxml/shapes/connector.py
@@ -6,7 +6,6 @@ lxml custom element classes for shape-related XML elements.
 
 from __future__ import absolute_import
 
-from ..ns import qn
 from .shared import BaseShapeElement
 from ..xmlchemy import BaseOxmlElement, OneAndOnlyOne
 
@@ -15,9 +14,7 @@ class CT_Connector(BaseShapeElement):
     """
     A line/connector shape ``<p:cxnSp>`` element
     """
-    @property
-    def spPr(self):
-        return self.find(qn('p:spPr'))
+    spPr = OneAndOnlyOne('p:spPr')
 
 
 class CT_ConnectorNonVisual(BaseOxmlElement):
