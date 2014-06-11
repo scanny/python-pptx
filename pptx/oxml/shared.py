e042c14210c1eab17f7421c7062be379122c3760
Steve Canny <scanny@cisco.com>
2014-06-11 01:47:46 -0700

    oxml: remove dead pptx.oxml.shared.XmlString()
    Only the version in pptx.oxml.xmlchemy is used now.

--- pptx/oxml/shared.py
+++ pptx/oxml/shared.py
@@ -7,79 +7,12 @@ lxml elements.
 
 from __future__ import absolute_import
 
-import re
-
 from lxml import etree
 
 
 def serialize_part_xml(part_elm):
     xml = etree.tostring(part_elm, encoding='UTF-8', standalone=True)
     return xml
-
-
-class XmlString(unicode):
-    """
-    Provides string comparison override suitable for serialized XML that is
-    useful for tests.
-    """
-
-    # '    <w:xyz xmlns:a="http://ns/decl/a" attr_name="val">text</w:xyz>'
-    # |          |                                          ||           |
-    # +----------+------------------------------------------++-----------+
-    #  front      attrs                                     | text
-    #                                                     close
-
-    _xml_elm_line_patt = re.compile(
-        '( *</?[\w:]+)(.*?)(/?>)([^<]*</[\w:]+>)?'
-    )
-
-    def __eq__(self, other):
-        lines = self.splitlines()
-        lines_other = other.splitlines()
-        if len(lines) != len(lines_other):
-            return False
-        for line, line_other in zip(lines, lines_other):
-            if not self._eq_elm_strs(line, line_other):
-                return False
-        return True
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def _attr_seq(self, attrs):
-        """
-        Return a sequence of attribute strings parsed from *attrs*. Each
-        attribute string is stripped of whitespace on both ends.
-        """
-        attrs = attrs.strip()
-        attr_lst = attrs.split()
-        return sorted(attr_lst)
-
-    def _eq_elm_strs(self, line, line_2):
-        """
-        Return True if the element in *line_2* is XML equivalent to the
-        element in *line*.
-        """
-        front, attrs, close, text = self._parse_line(line)
-        front_2, attrs_2, close_2, text_2 = self._parse_line(line_2)
-        if front != front_2:
-            return False
-        if self._attr_seq(attrs) != self._attr_seq(attrs_2):
-            return False
-        if close != close_2:
-            return False
-        if text != text_2:
-            return False
-        return True
-
-    def _parse_line(self, line):
-        """
-        Return front, attrs, close, text 4-tuple result of parsing XML element
-        string *line*.
-        """
-        match = self._xml_elm_line_patt.match(line)
-        front, attrs, close, text = [match.group(n) for n in range(1, 5)]
-        return front, attrs, close, text
 
 
 # class _Tagname(object):
