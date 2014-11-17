57b55ec2291059e8f34504b9f78ef40a7e61f434
Karthik Vijayraghavan <karthik1024@gmail.com>
2014-11-17 01:39:56 -0800

    compat: refactor __str__ for Python 3

--- pptx/enum/base.py
+++ pptx/enum/base.py
@@ -285,7 +285,7 @@ class EnumValue(int):
         """
         The symbolic name and string value of this member, e.g. 'MIDDLE (3)'
         """
-        return "%s (%d)" % (self._member_name, self)
+        return "{0:s} ({1:d})".format(self._member_name, self)
 
 
 class ReturnValueOnlyEnumMember(EnumMember):
