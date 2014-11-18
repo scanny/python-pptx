edd195871bdc45f87daa58111049bffded50ad23
Steve Canny <scanny@cisco.com>
2014-11-17 22:14:44 -0800

    build: add Python 3 support to build files

--- setup.py
+++ setup.py
@@ -39,6 +39,9 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Topic :: Office/Business :: Office Suites',
     'Topic :: Software Development :: Libraries'
 ]
