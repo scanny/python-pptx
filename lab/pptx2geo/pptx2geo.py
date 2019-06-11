01024e367c85714f18579670b53c6177ba143b34
David Brooks <dave@bcs.co.nz>
2019-06-11 13:51:03 +1200

    Get slide range right...

--- lab/pptx2geo/pptx2geo.py
+++ lab/pptx2geo/pptx2geo.py
@@ -228,7 +228,7 @@ class SvgExtract(object):
         svg_maker = SvgMaker(self._slides[slide_number-1], slide_number, self._slide_size, self._args)
 
     def slides_to_svg(self):
-        for n in range(len(self._slides)):
+        for n in range(1, len(self._slides)+1):
             self.slide_to_svg(n)
 
 #===============================================================================
