8752ae7a703179d8234e89b84c4361cedc5cce88
Steve Canny <stcanny@gmail.com>
2021-03-23 12:44:17 -0700

    part: add EmbeddedPackagePart.factory()

--- pptx/parts/embeddedpackage.py
+++ pptx/parts/embeddedpackage.py
@@ -5,6 +5,7 @@
 "Package" in this context means another OPC package, i.e. a DOCX, PPTX, or XLSX "file".
 """
 
+from pptx.enum.shapes import PROG_ID
 from pptx.opc.constants import CONTENT_TYPE as CT
 from pptx.opc.package import Part
 
@@ -23,7 +24,52 @@ class EmbeddedPackagePart(Part):
         used to open the "file-type" of `object_blob`. The returned part contains the
         bytes of `object_blob` and has the content-type also determined by `prog_id`.
         """
+        # --- a generic OLE object has no subclass ---
+        if prog_id not in PROG_ID:
+            return cls(
+                package.next_partname("/ppt/embeddings/oleObject%d.bin"),
+                CT.OFC_OLE_OBJECT,
+                object_blob,
+                package,
+            )
+
+        # --- A Microsoft Office file-type is a distinguished package object ---
+        EmbeddedPartCls = {
+            PROG_ID.DOCX: EmbeddedDocxPart,
+            PROG_ID.PPTX: EmbeddedPptxPart,
+            PROG_ID.XLSX: EmbeddedXlsxPart,
+        }[prog_id]
+
+        return EmbeddedPartCls.new(object_blob, package)
+
+    @classmethod
+    def new(cls, blob, package):
+        """Return new |EmbeddedPackagePart| subclass object.
+
+        The returned part object contains `blob` and is added to `package`.
+        """
         raise NotImplementedError
+
+
+class EmbeddedDocxPart(EmbeddedPackagePart):
+    """A Word .docx file stored in a part.
+
+    This part-type arises when a Word document appears as an embedded OLE-object shape.
+    """
+
+    partname_template = "/ppt/embeddings/Microsoft_Word_Document%d.docx"
+    content_type = CT.WML_DOCUMENT
+
+
+class EmbeddedPptxPart(EmbeddedPackagePart):
+    """A PowerPoint file stored in a part.
+
+    This part-type arises when a PowerPoint presentation (.pptx file) appears as an
+    embedded OLE-object shape.
+    """
+
+    partname_template = "/ppt/embeddings/Microsoft_PowerPoint_Presentation%d.pptx"
+    content_type = CT.PML_PRESENTATION
 
 
 class EmbeddedXlsxPart(EmbeddedPackagePart):
--- tests/parts/test_embeddedpackage.py
+++ tests/parts/test_embeddedpackage.py
@@ -2,11 +2,65 @@
 
 """Test suite for `pptx.parts.embeddedpackage` module."""
 
+import pytest
+
+from pptx.enum.shapes import PROG_ID
 from pptx.opc.constants import CONTENT_TYPE as CT
 from pptx.opc.package import OpcPackage, PackURI
-from pptx.parts.embeddedpackage import EmbeddedXlsxPart
+from pptx.parts.embeddedpackage import (
+    EmbeddedPackagePart,
+    EmbeddedDocxPart,
+    EmbeddedPptxPart,
+    EmbeddedXlsxPart,
+)
 
-from ..unitutil.mock import ANY, initializer_mock, instance_mock
+from ..unitutil.mock import ANY, class_mock, initializer_mock, instance_mock
+
+
+class DescribeEmbeddedPackagePart(object):
+    """Unit-test suite for `pptx.parts.embeddedpackage.EmbeddedPackagePart` objects."""
+
+    @pytest.mark.parametrize(
+        "prog_id, EmbeddedPartCls",
+        (
+            (PROG_ID.DOCX, EmbeddedDocxPart),
+            (PROG_ID.PPTX, EmbeddedPptxPart),
+            (PROG_ID.XLSX, EmbeddedXlsxPart),
+        ),
+    )
+    def it_provides_a_factory_that_creates_a_package_part_for_MS_Office_files(
+        self, request, prog_id, EmbeddedPartCls
+    ):
+        object_blob_ = b"0123456789"
+        package_ = instance_mock(request, OpcPackage)
+        embedded_object_part_ = instance_mock(request, EmbeddedPartCls)
+        EmbeddedPartCls_ = class_mock(
+            request, "pptx.parts.embeddedpackage.%s" % EmbeddedPartCls.__name__
+        )
+        EmbeddedPartCls_.new.return_value = embedded_object_part_
+
+        ole_object_part = EmbeddedPackagePart.factory(prog_id, object_blob_, package_)
+
+        EmbeddedPartCls_.new.assert_called_once_with(object_blob_, package_)
+        assert ole_object_part is embedded_object_part_
+
+    def but_it_creates_a_generic_object_part_for_non_MS_Office_files(self, request):
+        progId = "Foo.Bar.42"
+        object_blob_ = b"0123456789"
+        package_ = instance_mock(request, OpcPackage)
+        _init_ = initializer_mock(request, EmbeddedPackagePart, autospec=True)
+        partname_ = instance_mock(request, PackURI)
+        package_.next_partname.return_value = partname_
+
+        ole_object_part = EmbeddedPackagePart.factory(progId, object_blob_, package_)
+
+        package_.next_partname.assert_called_once_with(
+            "/ppt/embeddings/oleObject%d.bin"
+        )
+        _init_.assert_called_once_with(
+            ANY, partname_, CT.OFC_OLE_OBJECT, object_blob_, package_
+        )
+        assert isinstance(ole_object_part, EmbeddedPackagePart)
 
 
 class DescribeEmbeddedXlsxPart(object):
