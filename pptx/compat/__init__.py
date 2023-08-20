9760472c61133c0d8d5bc7c7fb37f2dac34f71da
Steve Canny <stcanny@gmail.com>
2023-08-19 20:49:06 -0700

    fix: collections.abc Windows 3.10 breakage

--- pptx/compat/__init__.py
+++ pptx/compat/__init__.py
@@ -4,32 +4,38 @@
 
 import sys
 
-import collections
-
 try:
-    Container = collections.abc.Container
-    Mapping = collections.abc.Mapping
-    Sequence = collections.abc.Sequence
-except AttributeError:
-    Container = collections.Container
-    Mapping = collections.Mapping
-    Sequence = collections.Sequence
+    from collections.abc import Container, Mapping, Sequence
+except ImportError:
+    from collections import Container, Mapping, Sequence
 
 if sys.version_info >= (3, 0):
     from .python3 import (  # noqa
         BytesIO,
+        Unicode,
         is_integer,
         is_string,
         is_unicode,
         to_unicode,
-        Unicode,
     )
 else:
     from .python2 import (  # noqa
         BytesIO,
+        Unicode,
         is_integer,
         is_string,
         is_unicode,
         to_unicode,
-        Unicode,
     )
+
+__all__ = [
+    "BytesIO",
+    "Container",
+    "Mapping",
+    "Sequence",
+    "Unicode",
+    "is_integer",
+    "is_string",
+    "is_unicode",
+    "to_unicode",
+]
